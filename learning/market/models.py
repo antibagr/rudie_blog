7fdbb1747febdff0a80af3d000fc2c79c3010ff0
Anton <16188591+antibagr@users.noreply.github.com>
2020-11-14 07:10:08 +0300

    initiate new Django project

---
+++ learning/market/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class UserBaseModel(BaseModel):
+
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+
+class Author(UserBaseModel):
+
+    def __str__(self) -> str:
+        return " ".join([x for x in [self.first_name, self.last_name] if x])
+
+
+class Post(BaseModel):
+
+    title = models.CharField(max_length=100)
+    context = models.TextField()
+
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.context
+
+    class Meta:
+        ordering = ['title']
---
+++ learning/migrate.bat
@@ -0,0 +1,27 @@
+@echo off
+
+:: ========================================
+:: Easy way to make migrations on Django project
+
+:: Made for fun by Rudolf Nemov
+:: https://github.com/antibagr
+
+echo Starting migration ...
+
+set app_name=market
+
+
+for /F "tokens=* USEBACKQ" %%F IN (`python manage.py makemigrations %app_name%`) DO (
+	SET migration_status=%%F
+)
+
+if /i "%migration_status:~0,2%"=="No" (
+	echo No changes detected in "%app_name%"
+	goto EOF
+) else (
+	echo %migration_status%
+	python manage.py migrate
+)
+
+
+:EOF
